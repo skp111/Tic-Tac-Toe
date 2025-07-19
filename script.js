5d427c6d082bd831142b5349e482b22fbfd74e0d
Sameer Kumar Padhi <166988326+skp111@users.noreply.github.com>
2025-07-19 16:36:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ let boxes = document.querySelectorAll('.box')
 let body = document.querySelector('body')
 let icon = document.querySelector('#btn')
 
-icon.addEventListener('click',() => {
+icon.addEventListener('click', () => {
     body.classList.toggle('light')
-    if(body.classList.contains('light')){
+    if (body.classList.contains('light')) {
         body.style.backgroundColor = 'black'
         body.style.color = 'white'
-        icon.children[0].classList.replace('fa-moon','fa-sun')
+        icon.children[0].classList.replace('fa-moon', 'fa-sun')
         icon.style.color = 'yellow'
-        for(let box of boxes)
+        for (let box of boxes)
             box.style.boxShadow = '5px 10px 20px rgba(255,255,255,0.2)'
     }
-    else{
+    else {
         body.style.backgroundColor = 'white'
         body.style.color = 'black'
-        icon.children[0].classList.replace('fa-sun','fa-moon')
+        icon.children[0].classList.replace('fa-sun', 'fa-moon')
         icon.style.color = 'black'
-        for(let box of boxes)
+        for (let box of boxes)
             box.style.boxShadow = '5px 10px 20px rgba(0, 0, 0, 0.4)'
     }
 })
@@ -28,12 +28,12 @@ const winPatterns = [
 ]
 
 const checkWinner = () => {
-    for(let pattern of winPatterns){
+    for (let pattern of winPatterns) {
         let val1 = boxes[pattern[0]].innerText
         let val2 = boxes[pattern[1]].innerText
         let val3 = boxes[pattern[2]].innerText
-        if(val1 && val2 && val3){
-            if(val1 === val2 && val2 === val3){
+        if (val1 && val2 && val3) {
+            if (val1 === val2 && val2 === val3) {
                 return val1
             }
         }
@@ -48,29 +48,41 @@ for (let turn of turns) {
     })
 }
 
+let count = 0
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
         if (!currentChoice) {
             alert('Please choose X or O first!');
             return
         }
-        if (box.innerText !== '') 
+        if (box.innerText !== '')
             return
         // or box.disabled = true
 
-        currentChoice === 'X' ?  box.style.color = 'green' : box.style.color = 'red'
+        currentChoice === 'X' ? box.style.color = 'green' : box.style.color = 'red'
         box.innerText = currentChoice;
         currentChoice = currentChoice === 'X' ? 'O' : 'X';
 
-        if (checkWinner()){
+        let Draw_OR_Win = (msg) => {
             let resetBtn = document.querySelector('.reset-win')
-            resetBtn.innerText = `Player ${checkWinner()} Wins!!`
+            resetBtn.innerText = msg
             setTimeout(() => {
+                alert('Game will Restart click OK')
                 location.reload()
-            },3000)
-            setTimeout(() => {
-                alert('Game Restarts in 3 seconds')
-            },1000)
+            }, 500)
+        }
+
+        let msg = ''
+        if (checkWinner()) {
+            msg = `Player ${checkWinner()} Wins!!`
+            Draw_OR_Win(msg)
+        }
+        else {
+            count += 1
+            if (count === 9) {
+                msg = 'Game was Draw'
+                Draw_OR_Win(msg)
+            }
         }
     })
 })
